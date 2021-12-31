5bc345c57746bf90764655ea462af2e969340d72
unknown <iagojsantos@gmail.com>
2021-12-31 15:08:33 -0300

    Finish

--- myproject/crm/admin.py
+++ myproject/crm/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Phone
+
+@admin.register(Phone)
+class PhoneAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'phone')
+    search_fields = ('user__email',)
